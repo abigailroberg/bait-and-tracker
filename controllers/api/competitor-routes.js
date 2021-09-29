c0518faaddf35e8e970edf4567d63ce257fc8748
coderjake91 <jacob.b.frazer@gmail.com>
2021-09-29 14:27:27 -0500

    finished basic funcitonality for fish and competitor routes following CRUD actions, utilized sequalize query syntax

--- controllers/api/competitor-routes.js
+++ controllers/api/competitor-routes.js
@@ -6,29 +6,99 @@ const { Competitor, Fish } = require('../../models');
 //relational db structure = (" one-to-many 'Competitor' -> 'Fish' ")
 //================================================================================================
 
-//GET route for all competitors : /api/competitors
+//GET route for all competitors (including fish caught information) : /api/competitors
 router.get('/', (req, res) => {
-
+    //get all competitors with corresponding fish caught for each competitor
+    Competitor.findAll({
+        /*attributes: [competitor attributes desired],
+        sub query-include: [fish attributes here, number of fish, biggest fish, etc.]*/
+    })
+    .then(dbCompetitorData => res.json(dbCompetitorData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
-//GET route for all competitors : /api/competitors/:id
+//GET route for a single competitors : /api/competitors/:id
 router.get('/:id', (req, res) => {
-
+    //get one competitor with corresponding fish caught data
+    Competitor.findOne({
+        where: {
+            id: req.params.id
+        }
+    /*attributes: [fish attributes here]*/
+    })
+    .then(dbCompetitorData => {
+        if(!dbCompetitorData){
+            res.status(404).json({ message: 'No competitor found with this id.'});
+            return;
+        }
+        res.json(dbCompetitorData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 //POST route to add new competitors : /api/competitors
-router.post('/', (req, res) => {
-
+router.post('/', ({body}, res) => {
+    //expect {name: 'STRING', email 'STRING', phone: 'STRING', username: 'STRING', password: 'STRING'}
+    Competitor.create({
+        name: body.name,
+        email: body.email,
+        phone: body.phone,
+        username: body.username,
+        password: body.password
+        //do we want to allow for the creation of fish with the creation of a new competitor?
+    })
+    .then(dbCompetitorData => res.json(dbCompetitorData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 //PUT route to update competitors by id : /api/competitors/:id
 router.put('/:id', (req, res) => {
-
+    //expect {name: 'STRING', email 'STRING', phone: 'STRING', username: 'STRING', password: 'STRING'}
+    Competitor.update( req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCompetitorData => {
+        if(!dbCompetitorData){
+            res.status(404).json({ message: 'No competitor found with this id.' });
+            return;
+        }
+        res.json(dbCompetitorData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 //DELETE route to delete competitors by id : /api/competitors/:id
 router.delete('/:id', (req, res) => {
-
+    Competitor.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCompetitorData => {
+        if(!dbCompetitorData) {
+            res.status(404).json({ message: 'No competitor found with this id.'});
+            return;
+        }
+        res.json(dbCompetitorData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
--- controllers/api/fish-routes.js
+++ controllers/api/fish-routes.js
@@ -8,27 +8,94 @@ const { Competitor, Fish } = require('../../models')
 
 //GET route for all fish caught : /api/fishCaught
 router.get('/', (req, res) => {
-
+    //get all fish caught with corresponding competitor usernames
+    Fish.findAll({
+        /*sub query-incldue: [competitor username here]*/
+    })
+    .then(dbFishData => res.json(dbFishData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 //GET route for a single fish caught : /api/fishCaught/:id
 router.get('/:id', (req, res) => {
-
+    //get one fish with corresponding competitor username
+    Fish.findOne({
+        where: {
+            id: req.params.id
+        }
+        /*sub query-include: [competitor username]*/
+    })
+    .then(dbFishData => {
+        if(!dbFishData){
+            res.status(404).json({ message: 'No fish found with that id.'});
+            return;
+        }
+        res.json(dbFishData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 //POST route to add new fish caught : /api/fishCaught
-router.post('/', (req, res) => {
-
+router.post('/', ({body}, res) => {
+ //expect {length: 'DECIMAL', weight 'DECIMAL', competitor_id: 'INTEGER', picture: 'TEXT'}
+    Fish.create({
+        length: body.length,
+        weight: body.weight,
+        competitor_id: body.competitor_id,
+        picture: body.picture
+    })
+    .then(dbFishData => res.json(dbFishData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
-//PUT route to update fish caught by id : /api/fishCaught/:id
+//PUT route to update fish caught by id (competitor fish affiliation) : /api/fishCaught/:id
 router.put('/:id', (req, res) => {
-
+//expect {length: 'DECIMAL', weight 'DECIMAL', competitor_id: 'INTEGER', picture: 'TEXT'}
+    Fish.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbFishData => {
+        if(!dbFishData){
+            res.status(404).json({ message: 'No fish found with that id.'});
+            return;
+        }
+        res.json(dbFishData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 });
 
 //DELETE route to delete fish caught by id : /api/fishCaught/:id
 router.delete('/:id', (req, res) => {
-
+    Fish.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbFishData => {
+        if(!dbFishData){
+            res.status(404).json({ message: 'No fish found with that id'});
+            return;
+        }
+        res.json(dbFishData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
