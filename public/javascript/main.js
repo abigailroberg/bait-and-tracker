aeb6c6f3f94aa79c3d8aa74c5abf01e3e3a16945
kimcc <15278020+kimcc@users.noreply.github.com>
2021-10-02 14:49:02 -0500

    organize main.js file and add comments

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,13 +1,24 @@
 // Create new date object and format it
-const date = new Date().toLocaleDateString('en-us', { year:"numeric", month:"short", day:"numeric" }); 
+const addDate = function() {
+  const date = new Date().toLocaleDateString('en-us', { year:"numeric", month:"short", day:"numeric" }); 
 
-document.getElementById("date").innerHTML = date;
+  document.getElementById("date").innerHTML = date;
+}
 
-const currentPage = location.href;
-const navItems = document.getElementsByClassName('navbar-link');
-
-for (let i = 0; i < navItems.length; i++) {
-  if(navItems[i].href === currentPage) {
-    navItems[i].classList.add("navbar-link-border");
+// Add a line under a nav link to indicate the current page.Loop through all the navbar links and add the navbar-link-border class if its href matches with the current page
+const addNavBorder = function() {
+  const currentPage = location.href;
+  const navItems = document.getElementsByClassName('navbar-link');
+  
+  for (let i = 0; i < navItems.length; i++) {
+    if(navItems[i].href === currentPage) {
+      navItems[i].classList.add("navbar-link-border");
+    }
   }
 }
+
+window.onload = function() {
+  addDate();
+  addNavBorder();
+}
+
