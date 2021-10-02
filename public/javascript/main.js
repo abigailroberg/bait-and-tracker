b0bef82b4365bc23a61d4cf503bc350bae191b20
kimcc <15278020+kimcc@users.noreply.github.com>
2021-10-02 13:40:12 -0500

    add fish image, table styling, fix link styling and add underline

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,3 +2,16 @@
 const date = new Date().toLocaleDateString('en-us', { year:"numeric", month:"short", day:"numeric" }); 
 
 document.getElementById("date").innerHTML = date;
+
+const currentPage = location.href;
+const navItems = document.getElementsByClassName('navbar-link');
+//const navLength = navItems.length;
+
+//console.log(navLength);
+console.log(navItems[0].href);
+
+for (let i = 0; i < navItems.length; i++) {
+  if(navItems[i].href === currentPage) {
+    navItems[i].classList.add("navbar-link-border");
+  }
+}
