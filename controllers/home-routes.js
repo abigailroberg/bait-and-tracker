6c6f09482ff22f719708feb4050efbbde00e245b
abraham perez reyes <abrahampre27@gmail.com>
2021-10-03 14:45:44 -0500

    handlebar for dashboard, need to fix leaderboard by fish and desc order

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,61 +1,16 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const {Competitor, Fish} = require ('../models')
-// TODO: Need to require models
-
-// router.get('/', (req, res) => {
-//   const fishes = [
-//     {
-//       id: 1, 
-//       length: 10,
-//       weight: 5,
-//       pictures: [
-//         'http://placekitten.com/48/36',
-//         'http://placekitten.com/48/36',
-//         'http://placekitten.com/48/36',
-//         'http://placekitten.com/48/36',
-//         'http://placekitten.com/48/36'
-//       ]
-//     },
-//     {
-//       id: 2, 
-//       length: 12,
-//       weight: 8,
-//       pictures: [
-//         'http://placekitten.com/48/36',
-//         'http://placekitten.com/48/36',
-//         'http://placekitten.com/48/36'
-//       ]
-//     }
-//   ]
-
-//   res.render('homepage', {
-//     fishes
-//   });
-// });
-
-  // .then(dbFishData => {
-  //   const fishes = dbFishData.map(fish => fish.get({ plain: true }));
-
-  //   res.render('homepage', {
-  //     fishes
-  //   });
-  // })
-  // .catch(err => {
-  //   console.log(err);
-  //   res.status(500).json(err);
-  // });
-//
 
 
 router.get('/', (req, res) => {
   //get all fish caught with corresponding competitor usernames
   Fish.findAll({
-      attributes: ['length', 'weight', 'picture', 'competitor_id', 'created_at'],
+      attributes: ['id','length', 'weight', 'picture', 'competitor_id', 'created_at'],
       include: [
           {
               model: Competitor,
-              attributes: ['name']
+              attributes: ['name','id']
           }
       ]
   })
@@ -70,7 +25,67 @@ router.get('/', (req, res) => {
 
 });
 
+// For competitor-profile
 
+router.get('/competitor/:id', (req, res) => {
+  //get one competitor with corresponding fish caught data
+  Competitor.findOne({
+      where: {
+          id: req.params.id
+      },
+      attributes: ['id', 'name', 'email', 'phone', [sequelize.literal('(SELECT SUM(length) FROM fish WHERE competitor.id = fish.competitor_id)'), 'totalLength' ]],
+      include: [
+          {
+              model: Fish,
+              attributes: ['id', 'length', 'weight', 'picture','created_at'],
+          }
+      ]
+  })
+  .then(dbCompetitorData => {
+      if(!dbCompetitorData){
+          res.status(404).json({ message: 'No competitor found with this id.'});
+          return;
+      }
+      const competitor = dbCompetitorData.get({plain:true});
+      
+      res.render('competitor-profile',{competitor});
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
+});
 
+router.get('/leaderboard', (req, res) => {
+  //get all competitors with corresponding fish caught for each competitor
+  Competitor.findAll({
+      attributes: ['name', 
+                  [sequelize.literal('(SELECT COUNT(competitor_id) FROM fish WHERE competitor.id = fish.competitor_id)'), 'totalFish' ],
+                  [sequelize.literal('(SELECT SUM(length) FROM fish WHERE competitor.id = fish.competitor_id)'), 'totalLength' ]],
+      
+      include: [
+          {
+              model: Fish,
+              attributes: ['length', 'picture', 'created_at', 'updated_at']
+          }
+      ]
+      // ,
+      // order: ['totalLength', 'DESC']
+  }) 
+  .then(dbCompetitorData => {
+      //serialize the sequalize object
+      const competitors = dbCompetitorData.map(competitor => competitor.get({ plain: true }));
+
+      //test this in insomnia...
+      res.render('leaderboard',{competitors});
+      //const rankedCompetitors = rankCompetitors(competitors);
+
+      //res.render('dashboard', {rankedCompetitors});
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(controllers);
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: false}).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 })
 
