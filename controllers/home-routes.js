5d136157efe0c93fd220416cd5a5debaf6713a8c
Abby Roberg <abbyroberg@gmail.com>
2021-10-04 17:21:50 -0500

    get route for fish totals by competitor

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,44 @@ const router = express.Router();
 
 //render app homepage
 router.get('/', (req, res) => {
-    res.render('homepage');
-});
+    Competitor.findAll({
+        attributes: ['id', 'name', 'email', 'phone'],
+        include: [
+            {
+                model: Fish,
+                attributes: ['length', 'weight', 'picture']
+            }
+        ]
+    })
+    .then(dbCompetitorData => {
+        let competitors = []
+        for(let i=0; i<dbCompetitorData.length; i++) {
+            let fish = dbCompetitorData[i].get({ plain: true })  
+            let totalLength = 0
+            let totalWeight = 0
+            let fishCount = 0
+            for(let i=0; i<fish.fishes.length; i++) {
+                totalLength = totalLength + Number(fish.fishes[i].length)
+                totalWeight = totalWeight + Number(fish.fishes[i].weight)
+                fishCount++
+            }
+            const totals = {
+                'id': dbCompetitorData[i].id,
+                'name': dbCompetitorData[i].name,
+                'email': dbCompetitorData[i].email,
+                'phone': dbCompetitorData[i].phone,
+                'fish_caught': fishCount,
+                'total_length': totalLength,
+                'total_weight': totalWeight
+            }
+            competitors.push(totals)
+        }
+        res.render('homepage', competitors)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
 
 module.exports = router;
