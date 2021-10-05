723c80523ff829d3cf5cebb0bbd1fb18813147c3
Abby Roberg <abbyroberg@gmail.com>
2021-10-05 13:54:48 -0500

    profile render refactor

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -77,4 +77,46 @@ router.get('/signup', (req, res) => {
     res.render('signup')
 })
 
+// profile page display
+router.get('/competitor/:id', (req, res) => {
+    //get one competitor with corresponding fish caught data
+    Competitor.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id', 
+            'name', 
+            'email', 
+            'phone', 
+            [sequelize.literal('(SELECT SUM(length) FROM fish WHERE competitor.id = fish.competitor_id)'), 'totalLength' ]
+        ],
+        include: [
+            {
+                model: Fish,
+                attributes: [
+                    'id', 
+                    'length', 
+                    'weight', 
+                    'picture',
+                    'created_at'
+                ]
+            }
+        ]
+    })
+    .then(dbCompetitorData => {
+        if(!dbCompetitorData){
+            res.status(404).json({ message: 'No competitor found with this id.'});
+            return;
+        }
+        const competitor = dbCompetitorData.get({ plain:true });
+        
+        res.render('profile', { competitor });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+  });
+
 module.exports = router;
