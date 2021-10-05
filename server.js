75a371bbeaab92b22d7ca0561d494267851236cb
Abby Roberg <abbyroberg@gmail.com>
2021-10-04 20:02:37 -0500

    fixed server.js

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const path = require('path');
 const express = require('express');
-
-const controllers = require('./controllers');
-
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-
-const sequelize = require('./config/connection');
-const sequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const sequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
@@ -23,11 +21,9 @@ const sess = {
 
 app.use(session(sess));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const hbs = exphbs.create({})
 
 // set up handlebars as the templating engine
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -36,9 +32,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
-app.use(controllers);
+app.use(require('./controllers'));
 
 // turn on connection to db and server
-sequelize.sync({ force: false}).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 })
